9cdcaa26171a5f9e080cb1495c586818f15e45eb
jinxm <jinxm@gorkor.com>
2017-08-19 16:20:15 +0800

    Initial commit

---
+++ src/components/page01.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>page01</h1>
+    <span>这是简单的路由页面，无任何功能。</span>
+    <div>
+      <input v-model='message'>
+      <p>{{message | reverseString}}</p>
+    </div>
+    <div>
+      <textarea v-model="content" :maxlength="total"></textarea>
+      <p>你还可以输入{{reduce}}</p>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        message : '',
+        total: 100,
+        content: ''
+      }
+    },
+    computed :{
+      reduce (){
+        return this.total - this.content.length
+      }
+    }
+    ,
+    filters : {
+      reverseString (value) {
+        if(!value) return '';
+        value = value.split('').reverse().join('');
+        return value
+      }
+    }
+  }
+</script>
